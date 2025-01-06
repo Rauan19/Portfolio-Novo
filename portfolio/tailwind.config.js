f64eceb931a819c387bcb1599a45a31b74bccc5c
Rauan <rauanconceicao789@gmail.com>
2025-01-06 15:23:30 -0300

    tewste

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from "tailwindcss-animate";
+
 export default {
   content: [
     "./pages/**/*.{js,jsx}",
@@ -58,5 +60,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 };
